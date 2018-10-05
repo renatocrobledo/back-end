6292b5dd73b55c28489b0bd4bfac665a84a1c0f4
Renato <renatocrobledo@gmail.com>
2018-10-04 21:58:35 -0500

    POO

---
+++ POO/dog_class.py
@@ -0,0 +1,19 @@
+
+class Dog:
+  def __init__(self, name, breed, color, size, weight):
+    self.name = name
+    self.breed = breed
+    self.color = color
+    self.size = size
+    self.weight = weight
+    print(f"Hello, I'm {name} and I'm alive and my size is {size}!")
+
+  def bark(self, sound = "Wwwhoof!"):
+   # print(self.name + " says: "  + sound)
+    print(f"{self.name} says: {sound}")
+
+  def eat(self, type_of_food = 'meat'):
+    print(f"{self.name} is eating {type_of_food}, and its weight is {self.weight}")
+
+  def my_weight(self):
+    return f"I'm {self.name} and my weight is: {self.weight}"
--- POO/dogs.py
+++ POO/dogs.py
@@ -1,27 +1,9 @@
-import dogs_description
-
-class Dog:
-  def __init__(self, name, breed, color, size, weight):
-    self.name = name
-    self.breed = breed
-    self.color = color
-    self.size = size
-    self.weight = weight
-    print(f"Hello, I'm {name} and I'm alive and my size is {size}!")
-
-  def bark(self, sound = "Wwwhoof!"):
-   # print(self.name + " says: "  + sound)
-    print(f"{self.name} says: {sound}")
-
-  def eat(self, type_of_food = 'meat'):
-    print(f"{self.name} is eating {type_of_food}, and its weight is {self.weight}")
-
-  def my_weight(self):
-    return f"I'm {self.name} and my weight is: {self.weight}"
+from dogs_description import dogs_list
+from dog_class import Dog
 
 my_dog_list = []
 
-for description in dogs_description.dogs_list:
+for description in dogs_list:
     new_dog = Dog(**description)
     my_dog_list.append(new_dog)
 
