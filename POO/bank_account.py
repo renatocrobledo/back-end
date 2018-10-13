e7a1f04ce653be3fed62055254e8106de6300fa6
Renato <renatocrobledo@gmail.com>
2018-10-12 21:26:34 -0500

    wip

--- POO/bank_account.py
+++ POO/bank_account.py
@@ -16,44 +16,78 @@ Definir un menú que simbolice la cuenta de banco con opciones:
 Y obviamente hacer que funcione :)
 '''
 
-def parse_to_integer(str_number):
+def parse_to_float(str_number):
     try:
       return float(str_number)
     except ValueError:
       return print('ups! solo numeros son admitidos!')
 
+ACCOUNT_ID = 0
+
+account_0 = None
+account_1 = None
+
 class Account():
   def __init__(self, balance = 0):
       self.balance = balance
+
   def deposit(self, quantity):
-    value = parse_to_integer(quantity)
+    value = parse_to_float(quantity)
     if value:
       self.balance += value
+    return True
   def withdraw(self, quantity):
-    value = parse_to_integer(quantity)
+    value = parse_to_float(quantity)
     if value:
       if value > self.balance:
         return print('metase a chambear machin!')
       self.balance -= value
+      return True
   def show_balance(self):
     print(f"Tienes ${self.balance} en tu cuenta")
+  def transfer(self, quantity):
+      if self.id == '0':
+        if account_0.withdraw(quantity):
+          account_1.deposit(quantity)
+      else:
+        if account_1.withdraw(quantity):
+          account_0.deposit(quantity)
 
-def menu():
-  return input(' [d] Depositar \n [r] Retirar \n [m] Mostrar Balance \n [s] Salir \n')
-
+account_0 = Account()
 account_1 = Account()
 
+def menu():
+  return input(' [d] Depositar \n [r] Retirar \n [m] Mostrar Balance \n [t] Transferir \n [s] Salir \n')
+
+def select_card():
+   return input(' [0] account_0 \n [1] account_1 [s] Salir')
+
 while True:
-  option = menu()
-  if option == 's':
-     print('Bye!')
-     break
-  elif option == 'd':
-    account_1.deposit(input('how much? '))
-  elif option == 'r':
-    account_1.withdraw(input('how much? '))
-  elif option == 'm':
-    account_1.show_balance()
+
+  card_id = select_card()
+
+  if card_id == 's':
+    print('Bye!')
+    break
+
+  if card_id != '0' or card_id != '1':
+    print('Ups no existe esa tarjeta')
+    continue
+  selected_card = account_0 if  card_id == '0' else account_1
+
+  while True:
+    option = menu()
+    if option == 's':
+      print('Bye!')
+      break
+    elif option == 'd':
+      selected_card.deposit(input('how much? '))
+    elif option == 'r':
+      selected_card.withdraw(input('how much? '))
+    elif option == 'm':
+      selected_card.show_balance()
+    elif option == 't':
+      selected_card.transfer(input('how much? '))
 
 
 
